83156e6f971e08fc0148da6c33ef5051cf9687e6
Peter Murphy <peter.francis.murphy@gmail.com>
2019-06-26 09:11:33 -0400

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/peter_murphy_simspace_challenge/precache-manifest.fadb80f9aa5b65aaede75bdbf6ce63ae.js"
+  "/peter_murphy_simspace_challenge/precache-manifest.5fd7039f32dfee3b63577004e0fdf3f9.js"
 );
 
 self.addEventListener('message', (event) => {
