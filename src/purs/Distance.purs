b330886862fbb0b347ff32b1cece53837235fee2
Peter Murphy <pete@simspace.com>
2019-10-13 13:22:18 -0400

    Less broken, but still broken

--- src/purs/Distance.purs
+++ src/purs/Distance.purs
@@ -4,12 +4,10 @@ import Prelude
 import Control.Monad.ST (ST)
 import Control.Monad.ST as ST
 import Control.Monad.ST.Ref as STRef
-import Data.Array (sortBy, unsafeIndex)
+import Data.Array (reverse, sortBy, unsafeIndex)
 import Data.Array.ST (STArray)
 import Data.Array.ST as STArray
-import Data.Char (toCharCode)
 import Data.Maybe (Maybe(..))
-import Data.Ord (abs)
 import Data.String (length)
 import Data.String.CodeUnits (toCharArray)
 import Partial.Unsafe (unsafePartial)
@@ -51,108 +49,38 @@ levenshtein ::
   String ->
   String ->
   Int
+levenshtein "" b = length b
+
+levenshtein a "" = length a
+
 levenshtein a b
   | a == b = 0
   | otherwise =
-    abs $ unsafePartial
+    unsafePartial
       $ ST.run do
           let
-            [ as, bs ] = toCharArray <$> sortBy (comparing length) [ a, b ]
-          la <- STRef.new (length a)
-          lb <- STRef.new (length b)
-          ST.while do
-            la' <- STRef.read la
-            lb' <- STRef.read lb
-            pure $ la' > 0 && as !!! (la' - 1) == bs !!! (lb' - 1)
-            $ STRef.modify (_ - 1) la
-            *> STRef.modify (_ - 1) lb
-          offset <- STRef.new 0
-          ST.while do
-            la' <- STRef.read la
-            offset' <- STRef.read offset
-            pure $ offset' < la' && as !!! offset' == bs !!! offset'
-            $ STRef.modify (_ + 1) offset
-          offset' <- STRef.read offset
-          _ <- STRef.modify (_ - offset') la
-          _ <- STRef.modify (_ - offset') lb
-          la' <- STRef.read la
-          lb' <- STRef.read lb
-          unless (la' == 0 || lb' < 3) do -- Could be a problem setting these all to zero
-            x <- STRef.new 0
-            y <- STRef.new 0
-            d0 <- STRef.new 0
-            d1 <- STRef.new 0
-            d2 <- STRef.new 0
-            d3 <- STRef.new 0
-            dd <- STRef.new 0
-            dy <- STRef.new 0
-            ay <- STRef.new 0
-            bx0 <- STRef.new 0
-            bx1 <- STRef.new 0
-            bx2 <- STRef.new 0
-            bx3 <- STRef.new 0
-            v <- STArray.empty
-            ST.for 0 la'
-              $ \i -> do
-                  _ <- STArray.push (i + 1) v
-                  _ <- STArray.push (toCharCode $ as !!! (offset' + i)) v
-                  pure unit
-            len <- STRef.new (la' * 2 - 1) -- This might fail??
-            ST.while do
-              x' <- STRef.read x
-              pure $ x' < lb' - 3
-              $ do
-                  x' <- STRef.read x
-                  d0' <- STRef.write x' d0
-                  bx0' <- STRef.write (toCharCode $ bs !!! (offset' + d0')) bx0
-                  d1' <- STRef.write (x' + 1) d1
-                  bx1' <- STRef.write (toCharCode $ bs !!! (offset' + d1')) bx1
-                  d2' <- STRef.write (x' + 2) d2
-                  bx2' <- STRef.write (toCharCode $ bs !!! (offset' + d2')) bx2
-                  d3' <- STRef.write (x' + 3) d3
-                  bx3' <- STRef.write (toCharCode $ bs !!! (offset' + d3')) bx3
-                  x_ <- STRef.modify (_ + 4) x
-                  dd' <- STRef.write x_ dd
-                  pure $ ST.for 0 ((la' * 2 - 1) `div` 2)
-                    $ \i -> do
-                        let
-                          z = i * 2
-                        z0 <- unsafePeek z v
-                        dy' <- STRef.write z0 dy
-                        z1 <- unsafePeek (z + 1) v
-                        ay' <- STRef.write (z1) ay
-                        d0'' <- STRef.modify (\d -> min' dy' d d1' bx0' ay') d0
-                        d1'' <- STRef.modify (\d -> min' d0'' d d2' bx1' ay') d1
-                        d2'' <- STRef.modify (\d -> min' d1'' d d3' bx2' ay') d2
-                        dd'' <- STRef.modify (\d -> min' d2'' d dd' bx3' ay') dd
-                        _ <- STArray.modify z (\_ -> dd'') v
-                        _ <- STRef.write d2'' d3
-                        _ <- STRef.write d1'' d2
-                        _ <- STRef.write d0'' d1
-                        _ <- STRef.write dy' d0
-                        _ <- STRef.write dd'' lb
-                        pure unit
-            ST.while do
-              x' <- STRef.read x
-              pure $ x' < lb'
-              $ do
-                  x' <- STRef.read x
-                  d0' <- STRef.write x' d0
-                  bx0' <- STRef.write d0' bx0
-                  x'' <- STRef.modify (_ + 1) x
-                  dd' <- STRef.write x'' dd
-                  pure $ ST.for 0 ((la' * 2 - 1) `div` 2)
-                    $ \i -> do
-                        let
-                          z = i * 2
-                        z0 <- unsafePeek z v
-                        dy' <- STRef.write z0 dy
-                        z1 <- unsafePeek (z + 1) v
-                        dd'' <- STRef.modify (\d -> min' dy' d0' d bx0' z1) dd
-                        _ <- STArray.modify z (\_ -> dd'') v
-                        _ <- STRef.write dy' d0
-                        pure unit
-            dd' <- STRef.read dd
-            _ <- STRef.write dd' lb
-            pure unit
-          STRef.read lb
+            [ as, bs ] = reverse $ toCharArray <$> sortBy (comparing length) [ a, b ]
+          row <- STArray.empty
+          prev <- STRef.new 0
+          val <- STRef.new 0
+          ST.for 0 (length a + 1) $ \i -> STArray.push i row
+          ST.for 1 (length b + 1)
+            $ \i -> do
+                _ <- STRef.write (i - 1) prev
+                _ <-
+                  ST.for 1 (length a + 1) \j -> do
+                    prev' <- STRef.read prev
+                    _ <-
+                      if (bs !!! i - 1 == as !!! j - 1) then do
+                        j1 <- unsafePeek (j - 1) row
+                        STRef.write j1 val
+                      else do
+                        j1 <- unsafePeek (j - 1) row
+                        j2 <- unsafePeek j row
+                        STRef.write (min (j1 + 1) (min (prev' + 1) (j2 + 1))) val
+                    _ <- STArray.modify (j - 1) (\_ -> prev') row
+                    val' <- STRef.read val
+                    STRef.write val' prev
+                prev' <- STRef.read prev
+                STArray.modify (length a) (\_ -> prev') row
+          unsafePeek (length a) row
